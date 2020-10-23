16212074d8acaf7a01e94127b58e36d66b3076ad
andrey zapolskiy <uniqo@yandex.by>
2020-10-23 15:11:01 +0300

    1.add landing pages to bot 2.changed telegram url to TestBotForWork

--- src/main/java/by/uniqo/telegrambot/webservivce/GreetingController.java
+++ src/main/java/by/uniqo/telegrambot/webservivce/GreetingController.java
@@ -8,15 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/index")
+    public String greeting() {
+        return "index";
     }
     @GetMapping("/")
-    public String mainPage(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    public String mainPage(){
+        return "index";
     }
 
 }
